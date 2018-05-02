98452949d0274aefc6f59175a63cd32f6b897d5e
Ze Chen <zeche@microsoft.com>
2018-05-02 14:45:07 -0700

    Use command to manually operate changes.

--- HadrBenchMark/Program.cs
+++ HadrBenchMark/Program.cs
@@ -12,13 +12,75 @@ namespace HadrBenchMark
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             HadrTestBase hadrTestBase = new HadrTestBase();
-            hadrTestBase.Setup();
-            hadrTestBase.TestNodesHADREnabled();
-            hadrTestBase.TestCreateAGWithTwoReplicasWithoutDatabase();
+
+            //try
+            //{
+            //    Console.WriteLine("Hello World!");
+
+                hadrTestBase.Setup();
+                bool Alive = true;
+                do
+                {
+                    string command = Console.ReadLine();
+                    // parse command
+                    string[] param = command.Split(' ');
+                    if (param != null)
+                    {
+                        switch (param[0])
+                        {
+                            case "AddDatabase":
+                                string dbNumber = param[1];
+                                int numberOfDB = 0;
+                                if (Int32.TryParse(dbNumber, out numberOfDB))
+                                {
+                                    hadrTestBase.AddDatabases(numberOfDB);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("invalid number.");
+                                }
+                                break;
+                            case "AddReplica":
+                                string arNumber = param[1];
+                                int numberOfAR = 0;
+                                if (Int32.TryParse(arNumber, out numberOfAR))
+                                {
+                                    CreateReplica(numberOfAR);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("invalid number.");
+                                }
+                                break;
+                            case "Cleanup":
+                                Alive = false;
+                                hadrTestBase.CleanUp();
+                                break;
+                            default:
+                                Console.WriteLine("No Valid parameter");
+                                break;
+                        }
+                    }
+
+                } while (Alive);
+            //}catch
+            //{
+            //    hadrTestBase.CleanUp();
+            //}
+
             Console.ReadKey();
-            hadrTestBase.CleanUp();
+            //hadrTestBase.CleanUp();
+        }
+
+        static void CreateReplica(int num)
+        {
+            Console.WriteLine("Add {0} Replica into AG", num);
+        }
+        static void CleanupEnv()
+        {
+            Console.WriteLine("Clean up environment.");
+
         }
     }
 }
