b6a9ea2aabdd94c256563e4a70e5f4032813794a
Ze Chen <zeche@microsoft.com>
2018-05-08 16:36:46 -0700

    traffic simulation

--- HadrBenchMark/simulatetraffic.sql
+++ HadrBenchMark/simulatetraffic.sql
@@ -3,84 +3,84 @@ go
 
 
 
-create procedure usp_generateIdentifier
-    @minLen int = 1
-    , @maxLen int = 128
-    , @seed int output
-    , @string varchar(8000) output
-as
-begin
-    set nocount on;
-    declare @length int;
-    declare @alpha varchar(8000)
-        , @digit varchar(8000)
-        , @specials varchar(8000)
-        , @first varchar(8000)
-    declare @step bigint = rand(@seed) * 2147483647;
+--create procedure usp_generateIdentifier
+--    @minLen int = 1
+--    , @maxLen int = 128
+--    , @seed int output
+--    , @string varchar(8000) output
+--as
+--begin
+--    set nocount on;
+--    declare @length int;
+--    declare @alpha varchar(8000)
+--        , @digit varchar(8000)
+--        , @specials varchar(8000)
+--        , @first varchar(8000)
+--    declare @step bigint = rand(@seed) * 2147483647;
 
-    select @alpha = 'qwertyuiopasdfghjklzxcvbnm'
-        , @digit = '1234567890'
-        , @specials = '_@# '
-    select @first = @alpha + '_@';
+--    select @alpha = 'qwertyuiopasdfghjklzxcvbnm'
+--        , @digit = '1234567890'
+--        , @specials = '_@# '
+--    select @first = @alpha + '_@';
 
-    set  @seed = (rand((@seed+@step)%2147483647)*2147483647);
+--    set  @seed = (rand((@seed+@step)%2147483647)*2147483647);
 
-    select @length = @minLen + rand(@seed) * (@maxLen-@minLen)
-        , @seed = (rand((@seed+@step)%2147483647)*2147483647);
+--    select @length = @minLen + rand(@seed) * (@maxLen-@minLen)
+--        , @seed = (rand((@seed+@step)%2147483647)*2147483647);
 
-    declare @dice int;
-    select @dice = rand(@seed) * len(@first),
-        @seed = (rand((@seed+@step)%2147483647)*2147483647);
-    select @string = substring(@first, @dice, 1);
+--    declare @dice int;
+--    select @dice = rand(@seed) * len(@first),
+--        @seed = (rand((@seed+@step)%2147483647)*2147483647);
+--    select @string = substring(@first, @dice, 1);
 
-    while 0 < @length 
-    begin
-        select @dice = rand(@seed) * 100
-            , @seed = (rand((@seed+@step)%2147483647)*2147483647);
-        if (@dice < 10) -- 10% special chars
-        begin
-            select @dice = rand(@seed) * len(@specials)+1
-                , @seed = (rand((@seed+@step)%2147483647)*2147483647);
-            select @string = @string + substring(@specials, @dice, 1);
-        end
-        else if (@dice < 10+10) -- 10% digits
-        begin
-            select @dice = rand(@seed) * len(@digit)+1
-                , @seed = (rand((@seed+@step)%2147483647)*2147483647);
-            select @string = @string + substring(@digit, @dice, 1);
-        end
-        else -- rest 80% alpha
-        begin
-            declare @preseed int = @seed;
-            select @dice = rand(@seed) * len(@alpha)+1
-                , @seed = (rand((@seed+@step)%2147483647)*2147483647);
+--    while 0 < @length 
+--    begin
+--        select @dice = rand(@seed) * 100
+--            , @seed = (rand((@seed+@step)%2147483647)*2147483647);
+--        if (@dice < 10) -- 10% special chars
+--        begin
+--            select @dice = rand(@seed) * len(@specials)+1
+--                , @seed = (rand((@seed+@step)%2147483647)*2147483647);
+--            select @string = @string + substring(@specials, @dice, 1);
+--        end
+--        else if (@dice < 10+10) -- 10% digits
+--        begin
+--            select @dice = rand(@seed) * len(@digit)+1
+--                , @seed = (rand((@seed+@step)%2147483647)*2147483647);
+--            select @string = @string + substring(@digit, @dice, 1);
+--        end
+--        else -- rest 80% alpha
+--        begin
+--            declare @preseed int = @seed;
+--            select @dice = rand(@seed) * len(@alpha)+1
+--                , @seed = (rand((@seed+@step)%2147483647)*2147483647);
 
-            select @string = @string + substring(@alpha, @dice, 1);
-        end
+--            select @string = @string + substring(@alpha, @dice, 1);
+--        end
 
-        select @length = @length - 1;   
-    end
-end
-go
-create proc [dbo].uspRandChars
-    @len int,
-    @min tinyint = 48,
-    @range tinyint = 74,
-    @exclude varchar(50) = '0:;<=>?@O[]`^\/',
-    @output varchar(50) output
-as 
-    declare @char char
-    set @output = ''
+--        select @length = @length - 1;   
+--    end
+--end
+--go
+--create proc [dbo].uspRandChars
+--    @len int,
+--    @min tinyint = 48,
+--    @range tinyint = 74,
+--    @exclude varchar(50) = '0:;<=>?@O[]`^\/',
+--    @output varchar(50) output
+--as 
+--    declare @char char
+--    set @output = ''
  
-    while @len > 0 begin
-       select @char = char(round(rand() * @range + @min, 0))
-       if charindex(@char, @exclude) = 0 begin
-           set @output += @char
-           set @len = @len - 1
-       end
-    end
-;
-go
+--    while @len > 0 begin
+--       select @char = char(round(rand() * @range + @min, 0))
+--       if charindex(@char, @exclude) = 0 begin
+--           set @output += @char
+--           set @len = @len - 1
+--       end
+--    end
+--;
+--go
 
 --declare @newpwd varchar(20)
 
@@ -109,7 +109,7 @@ select * from Production.Product
 declare @counter int
 set @counter = 1
 
-while @counter < 550000
+while 1=1
 begin
 
 
@@ -131,12 +131,12 @@ begin
 		declare @string varchar(128);
 
 		select @seed = RAND()*1000; -- saved start seed
-		print @seed
+
 
 		exec usp_generateIdentifier 
 			@seed = @seed output
 			, @string = @string output;
-		print @string;  
+
 		update Person.Password
 		set PasswordHash = @string
 			where BusinessEntityID = (select ROUND(RAND()*20777, 0));
@@ -159,7 +159,7 @@ begin
 	--end
 
 	set @counter = @counter +1
-	print @counter
+
 end
 
 
---
+++ TrafficSimulator/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+
+namespace TrafficSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            CancellationToken cancellationToken = cancellationTokenSource.Token;
+            ConcurrentQueue<Process> cq = new ConcurrentQueue<Process>();
+            Console.WriteLine("Hello World!");
+            List<string> dbList = new List<string>();
+            for (int i = 1; i < 11; i++)
+            {
+                string dbname = string.Format("DB_{0}", i);
+                dbList.Add(dbname);
+            }
+
+            foreach(string dbname in dbList)
+            {
+                var runningTask = Task.Factory.StartNew(() => CreateOstressTask(cq, dbname), cancellationToken);
+            }
+            Console.ReadKey();
+            cancellationTokenSource.Cancel();
+            KillOStressTask(cq);
+            Console.WriteLine("All task should be canceled.");
+            Console.ReadKey();
+
+
+        }
+        static public string DecoratePath(string path)
+        {
+            char doubleQuaote = '\"';
+            string ret = doubleQuaote + path + doubleQuaote;
+            return ret;
+        }
+        public static void CreateOstressTask(ConcurrentQueue<Process> cq, string dbName)
+        {
+            Process ostress = new Process();
+            ostress.StartInfo.FileName = @"C:\Program Files\Microsoft Corporation\RMLUtils\ostress.exe";
+            string queryPath = DecoratePath(@"C:\Users\zeche\Documents\Visual Studio 2017\Projects\HadrBenchMark\HadrBenchMark\test_noloop.sql");
+            string outputBase = @"C:\temp\";
+            string outputPath = DecoratePath(outputBase + dbName);
+            string argument = @"-Sze-bench-01\hadrbenchmark01 -d" +dbName + ' ' +  "-r100000 -q -i" + queryPath + " -o" + outputPath;
+            Console.WriteLine(DecoratePath(queryPath));
+            Console.WriteLine(DecoratePath(outputPath));
+            Console.WriteLine(argument);
+            ostress.StartInfo.Arguments = argument;
+            cq.Enqueue(ostress);
+            ostress.Start();
+        }
+        public static void KillOStressTask(ConcurrentQueue<Process> cq)
+        {
+            Process ostress;
+            while (cq.TryDequeue(out ostress))
+            {
+                if (!ostress.HasExited)
+                {
+                    ostress.Kill();
+                }
+            }
+        }
+    }
+}
